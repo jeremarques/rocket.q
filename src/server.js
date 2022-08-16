8386aa0586e7b15d1e191fe2af0ce101c86f37e1
Jeremias Marques <saimerejmarques@gmail.com>
2022-08-16 09:42:02 -0300

    version 3.0

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const route = require('./route')
+const path = require('path')
+
+const server = express()
+
+server.set('view engine', 'ejs')
+
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(route)
+
+server.listen(8080, () => console.log(`This server is online!`))
+
