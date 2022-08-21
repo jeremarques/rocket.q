1de9ca69ca6240ac48e787f965a230f5d38f5581
Jeremias Marques <saimerejmarques@gmail.com>
2022-08-21 10:49:45 -0300

    beta-version

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -1,6 +1,53 @@
+const Database = require('../db/config')
+
 module.exports = {
-  create(req, res) {
-    let roomId = 123456
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+    let roomId = ''
+    let isRoom = true 
+
+    while (isRoom) {
+      for(var i = 0; i < 6; i++) {
+        roomId += Math.floor(Math.random() * 10).toString()
+      }
+
+      const roomExistIds = await db.all(`SELECT id FROM rooms`)  
+      isRoom = roomExistIds.some(roomExistId => roomExistId === roomId)
+      
+      if (!isRoom) {
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
+    }
+
+
+    /* Inserção da sala no Database */
+
+
+    await db.close()
+
+    res.redirect(`/room/${roomId}`)
+
+    
+  },
+  async open(req, res) {
+    const db = await Database()
+
+    const roomId = req.params.room     
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+    
+    res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
+  },
+
+  enter(req, res) {
+    const roomId = req.body.roomId
 
     res.redirect(`/room/${roomId}`)
   }
--- src/route.js
+++ src/route.js
@@ -6,9 +6,12 @@ const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
-route.get('/room/:room', (req, res) => res.render("room"))
 
-route.post('/question/:room/:question/:action', questionController.index)
+route.get('/room/:room', roomController.open)
 route.post('/create-room', roomController.create)
+route.post('/enter-room', roomController.enter)
+
+route.post('/question/create/:room', questionController.create)
+route.post('/question/:room/:question/:action', questionController.index)
 
 module.exports = route
